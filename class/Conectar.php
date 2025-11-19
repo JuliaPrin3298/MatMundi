8e58cc621e99a93eeec075c97b0a5a46914ae0b0
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2025-11-19 01:30:22 -0300

    ATU: ajustando o conectar

--- class/Conectar.php
+++ class/Conectar.php
@@ -1,43 +1,14 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "matmundi";
 
-<<<<<<< HEAD
 $conexao = new mysqli($host, $user, $pass, $dbname);
-
 if ($conexao->connect_error) {
-    die("Erro de conexão: " . $conexao->connect_error);
-=======
-class Conectar extends PDO {
-
-    private static $instancia; // singleton
-
-    private $host = "localhost";
-    private $usuario = "root";
-    private $senha = "";
-    private $db = "matmundi"; // coloque seu DB correto
-
-    private function __construct() {
-        try {
-            parent::__construct("mysql:host=$this->host;dbname=$this->db;charset=utf8", $this->usuario, $this->senha);
-            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Erro de conexão: " . $e->getMessage());
-        }
-    }
-
-    // pega a instância única
-    public static function getInstance() {
-        if (!isset(self::$instancia)) {
-            self::$instancia = new Conectar();
-        }
-        return self::$instancia;
-    }
-
-    // executa query simples
-    public function sql($query, $params = []) {
-        $stmt = $this->prepare($query);
-        $stmt->execute($params);
-        return $stmt;
-    }
->>>>>>> a79907ff51adc113a712848bf23fde8e6c28d04d
+    error_log("Erro de conexão: " . $conexao->connect_error);
+    die("Erro ao conectar ao banco de dados."); // mensagem genérica
 }
+
 ?>
+
